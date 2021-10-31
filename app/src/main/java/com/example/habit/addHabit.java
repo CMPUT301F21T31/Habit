e56079fceaf14b1294ee7ba0299894788f749538
Chen74 <54929297+Chen74@users.noreply.github.com>
2021-10-30 22:47:36 -0600

    update round button

--- app/src/main/java/com/example/habit/addHabit.java
+++ app/src/main/java/com/example/habit/addHabit.java
@@ -2,13 +2,19 @@ package com.example.habit;
 
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.Month;
 
 
 /**
@@ -23,6 +29,13 @@ public class addHabit extends AppCompatActivity {
     ImageButton confirm;
     ImageButton startDate;
     ImageButton endDate;
+    Button Monday;
+    Button Tuesday;
+    Button Wednesday;
+    Button Thursday;
+    Button Friday;
+    Button Saturday;
+    Button Sunday;
     EditText startMonth;
     EditText startDay;
     EditText startYear;
@@ -35,6 +48,8 @@ public class addHabit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_habit);
 
+        final RelativeLayout relativeLayout;
+
         back = findViewById(R.id.back);
         confirm = findViewById(R.id.confirm);
         startDate = findViewById(R.id.start_date);
@@ -45,6 +60,66 @@ public class addHabit extends AppCompatActivity {
         endMonth = findViewById(R.id.end_month);
         endDay  = findViewById(R.id.end_day);
         endYear = findViewById(R.id.end_year);
+        Monday = findViewById(R.id.monday);
+        Tuesday = findViewById(R.id.tuesday);
+        Wednesday = findViewById(R.id.wednesday);
+        Thursday = findViewById(R.id.thursday);
+        Friday = findViewById(R.id.friday);
+        Saturday = findViewById(R.id.saturday);
+        Sunday = findViewById(R.id.sunday);
+
+
+        Monday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tuesday.setBackground(getDrawable(R.drawable.round_button_clicked));
+            }
+        });
+
+
+
+        Tuesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tuesday.setBackground(getDrawable(R.drawable.round_button_clicked));
+            }
+        });
+
+        Wednesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Wednesday.setBackground(getDrawable(R.drawable.round_button_clicked));
+            }
+        });
+
+        Thursday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thursday.setBackground(getDrawable(R.drawable.round_button_clicked));
+            }
+        });
+
+        Friday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Friday.setBackground(getDrawable(R.drawable.round_button_clicked));
+            }
+        });
+
+        Saturday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Saturday.setBackground(getDrawable(R.drawable.round_button_clicked));
+            }
+        });
+
+        Sunday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Sunday.setBackground(getDrawable(R.drawable.round_button_clicked));
+            }
+        });
+
 
 
         // Back to the main screen
