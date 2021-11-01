0b0df733cd49c79034e32a338d7344cac8a7756f
dadaqi <77340349+dadaqi@users.noreply.github.com>
2021-11-01 15:38:40 -0600

    pass the start date in store

--- app/src/main/java/com/example/habit/calendar.java
+++ app/src/main/java/com/example/habit/calendar.java
@@ -3,7 +3,11 @@ package com.example.habit;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
@@ -11,13 +15,25 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class calendar extends AppCompatActivity {
-
+Button calb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         initOverall();
+        calb=findViewById(R.id.calb);
+        calb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goback();
+            }
+        });
     }
+    public void goback(){ Intent intent=new Intent(this,tutorial_6time.class);
+        startActivity(intent);
+
+    }
+
 
     public void initOverall(){
         final TextView dateTest = (TextView)findViewById(R.id.convert);
@@ -28,7 +44,14 @@ public class calendar extends AppCompatActivity {
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
                 String selectedDates = sdf.format(new Date(year-1900,month,dayOfMonth));
                 dateTest.setText(selectedDates);
+                SharedPreferences prefs=getSharedPreferences("prefs",MODE_PRIVATE);
+                SharedPreferences.Editor editor=prefs.edit();
+                editor.putString("firstdate",selectedDates);
+                editor.apply();
             }
         });
+
     }
+
 }
+
--- app/src/main/java/com/example/habit/tutorial_6time.java
+++ app/src/main/java/com/example/habit/tutorial_6time.java
@@ -3,18 +3,26 @@ package com.example.habit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class tutorial_6time extends AppCompatActivity {
 Button t5b;
 
+
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial5time);
         t5b=findViewById(R.id.t5b);
+        textView2=findViewById(R.id.textView2);
+        SharedPreferences prefs=getSharedPreferences("prefs",MODE_PRIVATE);
+        String firstdate= prefs.getString("firstdate","");
+        textView2.setText(firstdate);
         t5b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
