1b4ea849159e5f43decebbc0ca49b62e789d2bef
Justin Boileau <jboileau@ualberta.ca>
2021-11-04 15:13:23 -0600

    Some Habit tests, issue #29

--- app/src/test/java/com/example/habit/HabitTests.java
+++ app/src/test/java/com/example/habit/HabitTests.java
@@ -4,15 +4,57 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class HabitTests {
 
+    Habit mockHabit1;
+    Habit mockHabit2;
+    Date now;
+    Date later;
+    HashMap<String, Boolean> daysOfWeek1;
+    HashMap<String, Boolean> daysOfWeek2;
+    ArrayList<String> events;
+
+    @BeforeAll
+    void setup() {
+
+        // Mock habit event strings
+        events = new ArrayList<String>();
+        events.add("3WamsQqMlSlEL7ONVsuA");
+        events.add("FMsioS8GjR4bjD3nbQbh");
+
+        // Mock occurence maps
+        try {
+            daysOfWeek1 = Habit.generateDaysDict(new ArrayList<Boolean>(
+                    Arrays.asList(false, true, false, false, true, false, false)));
+            daysOfWeek2 = Habit.generateDaysDict(new ArrayList<Boolean>(
+                    Arrays.asList(true, false, false, true, false, false, true)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Mock dates
+        now = new Date();
+        LocalDate d = LocalDate.parse("2022-11-01");
+        later = Date.from(d.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        // Create mock habits for testing using constructor with and without events
+        mockHabit1 = new Habit("Swimming", "Get fit", now, later, daysOfWeek1, events);
+        mockHabit2 = new Habit("Reading", "Learn", now, later, daysOfWeek2);
+
+    }
+
     @Test
-    public void testGenerateDaysDict() throws Exception {
+    void testGenerateDaysDict() throws Exception {
 
         ArrayList<Boolean> allDays = new ArrayList<Boolean>(Arrays.asList(true, true, true, true, true, true, true));
         ArrayList<Boolean> noDays = new ArrayList<Boolean>(Arrays.asList(false, false, false, false, false, false, false));
@@ -73,5 +115,103 @@ public class HabitTests {
         assertEquals(result2, noDaysExpected);
         assertEquals(result3, oneDayExpected);
         assertEquals(result4, manyDaysExpected);
+    }
+
+    @Test
+    void getTitle() {
+        assertEquals("Swimming", mockHabit1.getTitle());
+    }
+
+    @Test
+    void setTitle() {
+        mockHabit2.setTitle("Gymnastics");
+        assertNotEquals("Reading", mockHabit2.getTitle());
+        assertEquals("Gymnastics", mockHabit2.getTitle());
+    }
+
+    @Test
+    void getReason() {
+        assertEquals("Get fit", mockHabit1.getReason());
+    }
+
+    @Test
+    void setReason() {
+        mockHabit2.setReason("For fun");
+        assertNotEquals("Get fit", mockHabit2);
+        assertEquals("For fun", mockHabit2);
+    }
+
+    @Test
+    void getStart() {
+        assertEquals(now, mockHabit1.getStart());
+    }
+
+//    @Test
+//    void setStart() {
+//        mockHabit2.setStart();
+//        assertNotEquals("", mockHabit2);
+//        assertEquals("", mockHabit2);
+//    }
+
+    @Test
+    void getEnd() {
+        assertEquals(later, mockHabit1.getEnd());
+    }
+
+//    @Test
+//    void setEnd() {
+//        mockHabit2.set();
+//        assertNotEquals("", mockHabit2);
+//        assertEquals("", mockHabit2);
+//    }
+
+    @Test
+    void getHabitId() {
+        assertNull(mockHabit1.getHabitId());
+    }
+
+    @Test
+    void setHabitId() {
+        mockHabit2.setHabitId("jlk5432lkjf");
+        assertNotNull(mockHabit2.getHabitId());
+        assertEquals("jlk5432lkjf", mockHabit2.getHabitId());
+
+        // Habit ID shouldn't change once it is set
+        mockHabit2.setHabitId("fasfsafklj3");
+        assertNotEquals("fasfsafklj3", mockHabit2.getHabitId());
+        assertEquals("jlk5432lkjf", mockHabit2.getHabitId());
+    }
+
+    @Test
+    void getUserId() {
+        assertNull(mockHabit1.getUserId());
+    }
+
+//    @Test
+//    void setUserId() {
+//        mockHabit2.set();
+//        assertNotEquals("", mockHabit2);
+//        assertEquals("", mockHabit2);
+//    }
+
+    @Test
+    void getDaysOfWeek() {
+        assertEquals(daysOfWeek1, mockHabit1.getDaysOfWeek());
+    }
+
+//    @Test
+//    void setDaysOfWeek() {
+//        mockHabit2.set();
+//        assertNotEquals("", mockHabit2);
+//        assertEquals("", mockHabit2);
+//    }
+
+    @Test
+    void isOnDay() {
+    }
+
+    @Test
+    void getEvents() {
+        assertEquals(events, mockHabit1.getEvents());
     }
 }
