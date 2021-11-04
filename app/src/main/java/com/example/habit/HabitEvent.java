d072ff6315832c343df7d666b9e8ff1e5f49fb1e
Justin Boileau <jboileau@ualberta.ca>
2021-11-04 13:27:28 -0600

    Added habitId and habitEventId properties to HabitEvent class, for use in issue #15 #18 #19. Finished HabitEvent comments

--- app/src/main/java/com/example/habit/HabitEvent.java
+++ app/src/main/java/com/example/habit/HabitEvent.java
@@ -1,13 +1,17 @@
 package com.example.habit;
 
+import android.util.Log;
+
 /**
  * Class denoting an instance (event) of a particular Habit
  */
 public class HabitEvent {
 
     // private photo // TODO: How should we store photos?
-    private String location;
+    private String location; // TODO: Will have to change this to store an actual location
     private String comments;
+    private String habitId;
+    private String habitEventId;
 
     /**
      *
@@ -19,31 +23,72 @@ public class HabitEvent {
         this.comments = comments;
     }
 
-    /**
-     * @return Get location of this habit event
+    /** Get location of this habit event
+     * @return String location
      */
     public String getLocation() {
         return location;
     }
 
-    /**
-     * @param location New location for this habit event
+    /** Set location for this habit event
+     * @param location String location
      */
     public void setLocation(String location) {
         this.location = location;
     }
 
     /**
-     * @return Get the comments for this habit event
+     * Get the comments for this habit event
+     * @return String comments
      */
     public String getComments() {
         return comments;
     }
 
-    /**
-     * @param comments New comments for this habit event
+    /** Set comments string for this habit event
+     * @param comments String comments
      */
     public void setComments(String comments) {
         this.comments = comments;
+    }
+
+    /**
+     * Get the habitId for this HabitEvent
+     * @return String ID corresponding to that in firestore
+     */
+    public String getHabitId() {
+        return habitId;
+    }
+
+    /**
+     * Set habitId for this HabitEvent if not already set
+     * @param habitId String ID corresponding to that in firestore
+     */
+    public void setHabitId(String habitId) {
+        if (this.habitId != null) {
+            Log.e("USER ID SET ERROR", "Should not set the habitId of HabitEvent which already has an ID");
+        } else {
+            this.habitId = habitId;
+        }
+    }
+
+    /**
+     * Get the habitEventId of this habitEvent
+     * @return String ID corresponding to that in firestore
+     */
+    public String getHabitEventId() {
+        return habitEventId;
+    }
+
+    /**
+     * Set habitEventId if not already set
+     * @param habitEventId String ID corresponding to that in firestore
+     */
+    public void setHabitEventId(String habitEventId) {
+        if (this.habitEventId != null) {
+            Log.e("HABIT EVENT ID SET", "Should not set habitEventId of HabitEvent which already has an ID");
+        } else {
+            this.habitEventId = habitEventId;
+        }
     }
 }
