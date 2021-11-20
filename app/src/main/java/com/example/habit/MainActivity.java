cc94c2c6b1e1cfd02a549340326348db67cc4dd9
dadaqi <77340349+dadaqi@users.noreply.github.com>
2021-11-20 00:20:10 -0700

    permission request for map

--- app/src/main/java/com/example/habit/MainActivity.java
+++ app/src/main/java/com/example/habit/MainActivity.java
@@ -2,27 +2,49 @@ package com.example.habit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     Button fisrtstartrest;
-
+    boolean isPersmissionGranter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
         boolean firstStart = prefs.getBoolean("firstStart", true);
+        checkpermission();
+        if(isPersmissionGranter){
+            if(checkGoogleplaServices()){
+                Toast.makeText(this,"Google playservices Available",Toast.LENGTH_SHORT).show();
 
+            }
+            else{
+                Toast.makeText(this,"Google playservices NOT Available",Toast.LENGTH_SHORT).show();
+            }
+        }
 Toast.makeText(MainActivity.this,"firststart?"+String.valueOf(firstStart),Toast.LENGTH_LONG).show();
     if (true){ // always open for now
 
@@ -45,6 +67,39 @@ Toast.makeText(MainActivity.this,"firststart?"+String.valueOf(firstStart),Toast.
         splashwelcome();
             // openHabitList();
         }
+    }
+
+    private boolean checkGoogleplaServices() {
+        GoogleApiAvailability googleApiAvailability=GoogleApiAvailability.getInstance();
+        int result=googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if(result== ConnectionResult.SUCCESS){
+            return true;
+        }return false;
+    }
+
+    private void checkpermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                isPersmissionGranter=true;
+                Toast.makeText(MainActivity.this,"permission Granter",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri=Uri.fromParts("package",getPackageName(),"");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
     }
 
     private void opentutorial() {
