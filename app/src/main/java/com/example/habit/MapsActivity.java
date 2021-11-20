612f03392f94af7e41d9489fbe74711b2d483d58
dadaqi <77340349+dadaqi@users.noreply.github.com>
2021-11-20 01:16:39 -0700

    redo permission

--- app/src/main/java/com/example/habit/MapsActivity.java
+++ app/src/main/java/com/example/habit/MapsActivity.java
@@ -2,8 +2,15 @@ package com.example.habit;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,11 +18,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.habit.databinding.ActivityMapsBinding;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    boolean isPersmissionGranter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +42,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+        checkpermission();
+        if (isPersmissionGranter) {
+            if (checkGoogleplaServices()) {
+                Toast.makeText(this, "Google playservices Available", Toast.LENGTH_SHORT).show();
 
+            } else {
+                Toast.makeText(this, "Google playservices NOT Available", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -48,4 +70,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-}
+
+
+    private boolean checkGoogleplaServices() {
+        GoogleApiAvailability googleApiAvailability=GoogleApiAvailability.getInstance();
+        int result=googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if(result== ConnectionResult.SUCCESS){
+            return true;
+        }return false;
+    }
+
+    private void checkpermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                isPersmissionGranter = true;
+                Toast.makeText(MapsActivity.this, "permission Granter", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), "");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
+
+    }}
