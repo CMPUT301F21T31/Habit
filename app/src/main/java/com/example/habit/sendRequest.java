a32cdd7d550967c6d090e33936328e79c97336ad
Lewis <36083021+lewisning@users.noreply.github.com>
2021-11-21 22:52:25 -0700

    create send request dialog files

--- app/src/main/java/com/example/habit/sendRequest.java
+++ app/src/main/java/com/example/habit/sendRequest.java
@@ -1,11 +1,59 @@
 package com.example.habit;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Objects;
 
 /**
  * US 05.01.01
  */
-public class sendRequest {
+public class sendRequest extends DialogFragment {
 
+    private User email;
+    private User uid;
 
+    public sendRequest(User email, User uid) {
+        this.email = email;
+        this.uid = uid;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_send_friend_request, null);
+        EditText friendEmail = view.findViewById(R.id.request_email);
+        ImageButton back = view.findViewById(R.id.backFriendActivityButton);
+        ImageButton confirmSend = view.findViewById(R.id.confirmRequestButton);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Objects.requireNonNull(getDialog()).dismiss();
+            }
+        });
+
+        confirmSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getEmail = friendEmail.getText().toString();
+
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder.setView(view).create();
+    }
 }
