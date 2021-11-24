996d23404bcc8d0927eac5037b7c454efdafd91f
dadaqi <77340349+dadaqi@users.noreply.github.com>
2021-11-23 18:59:20 -0700

    test

--- app/src/main/java/com/example/habit/tutorial_1.java
+++ app/src/main/java/com/example/habit/tutorial_1.java
@@ -3,8 +3,6 @@ package com.example.habit;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,37 +10,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class tutorial_1 extends AppCompatActivity {
-    Button t_1b;
+    ImageButton t_1b;
     Button skipToLoginButton;
     EditText t_1t;
-    Button testmap;
-    Button cameratest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial1);
-        t_1t = findViewById(R.id.t_1t);
-        t_1b = findViewById(R.id.t_1b);
-        testmap = findViewById(R.id.test_map);
+        t_1t=findViewById(R.id.t_1t);
+        t_1b=findViewById(R.id.t_1b);
         skipToLoginButton = findViewById(R.id.goTo_login_button);
-        cameratest=findViewById(R.id.cameratest);
-        cameratest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(tutorial_1.this, camera.class);
-                startActivity(intent);
-            }
-        });
-        testmap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                gotomap();
-            }
-        });
+
 
         //Toast.makeText(MainActivity.this,"firststart?"+String.valueOf(firstStart),Toast.LENGTH_LONG).show();
 
@@ -50,11 +33,11 @@ public class tutorial_1 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = t_1t.getText().toString();
-                SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("firstStartname", name);
+                SharedPreferences prefs=getSharedPreferences("prefs",MODE_PRIVATE);
+                SharedPreferences.Editor editor=prefs.edit();
+                editor.putString("firstStartname",name);
                 editor.apply();
-                String inname = prefs.getString("firstStartname", "");
+                String inname= prefs.getString("firstStartname","");
                 //String uniqueID =prefs.getString("UID","");
                 //Toast.makeText(tutorial_1.this,inname+uniqueID,Toast.LENGTH_LONG).show();
 
@@ -66,37 +49,18 @@ public class tutorial_1 extends AppCompatActivity {
         skipToLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(tutorial_1.this, splashscreen.class);
+                Intent intent = new Intent(tutorial_1.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
     }
 
-    private void gotomap() {
-
-        Intent intent = new Intent(this, MapsActivity.class);
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        String inname = prefs.getString("firstStartname", "");
+    private void opent_2() { Intent intent=new Intent(this,tutorial_2usern.class);
+        SharedPreferences prefs=getSharedPreferences("prefs",MODE_PRIVATE);
+        String inname= prefs.getString("firstStartname","");
 
         intent.putExtra("fname", inname);
         // startActivity(intent);
-        startActivity(intent);
-
-
-    }
-
-    private void opent_2() {
-        Intent intent = new Intent(this, tutorial_4habit.class);
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        String inname = prefs.getString("firstStartname", "");
-
-        intent.putExtra("fname", inname);
-        // startActivity(intent);
-        startActivity(intent);
-    }
-
-    private void splashwelcome() {
-        Intent intent = new Intent(tutorial_1.this, splashscreen.class);
         startActivity(intent);
     }
 }
