42621130ad34ca3016e56f55cbfced699b188dce
Lewis <36083021+lewisning@users.noreply.github.com>
2021-11-25 15:59:03 -0700

    FINISHED: view friends' public habits list and details

--- app/src/main/java/com/example/habit/FriendsHabit.java
+++ app/src/main/java/com/example/habit/FriendsHabit.java
@@ -1,8 +1,12 @@
 package com.example.habit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -37,6 +41,7 @@ public class FriendsHabit extends AppCompatActivity {
 
     TextView title;
     ListView habitList;
+    ImageButton back;
     ArrayList<Habit> dataList;
     HabitList habitListAdapter;
 
@@ -46,83 +51,90 @@ public class FriendsHabit extends AppCompatActivity {
         setContentView(R.layout.friends_habit);
 
         auth = FirebaseAuth.getInstance();
-        fb_user = auth.getCurrentUser();
         db = FirebaseFirestore.getInstance();
 
         title = findViewById(R.id.title_with_name);
         habitList = findViewById((R.id.habit_view_list));
+        back = findViewById((R.id.back));
 
         dataList = new ArrayList<>();
         habitListAdapter = new HabitList(this, dataList, false);
         habitList.setAdapter(habitListAdapter);
 
-        if (fb_user == null) {
-            // Go back to login
-        } else {
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-            // Get User record for logged in user
-            DocumentReference docRef = db
-                    .collection("users")
-                    .document(fb_user.getUid());
-            docRef
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
+        Intent intent = getIntent();
+        String userId = intent.getExtras().getString("user id");
 
-                            // Get user object and their habit IDs
-                            user = document.toObject(User.class);
-                            Habit habit = document.toObject(Habit.class);
-                            title.setText(user.getDisplayName() + "'s Habits");
+        // Get User record for logged in user
+        DocumentReference docRef = db
+                .collection("users")
+                .document(userId);
+        docRef
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
 
-                            ArrayList<String> habits = user.getHabits();
+                        // Get user object and their habit IDs
+                        user = document.toObject(User.class);
+                        Habit habit = document.toObject(Habit.class);
+                        title.setText(user.getDisplayName() + "'s Habits");
 
-                            // On change listener for habits belonging to this user
-                            db.collection("habits")
-                                    .whereEqualTo("userId", fb_user.getUid()) // Only get habits for this user
-                                    .whereEqualTo(String.valueOf(true), habit.getIfPublic())
-                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                                        @Override
-                                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        ArrayList<String> habits = user.getHabits();
 
-                                            // Log failure and exit
-                                            if (error != null) {
-                                                Log.w("FAILED", "Listen failed.", error);
-                                                return;
-                                            }
+                        // On change listener for habits belonging to this user
+                        db.collection("habits")
+                                .whereEqualTo("userId", userId) // Only get habits for this user
+                                .whereEqualTo("ifPublic", true)
+                                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
 
-                                            // Get today's weekday so we know which habits to add to today list
-                                            String weekday = new SimpleDateFormat("EEEE").format(new Date()).toString();
-                                            Log.i("Today", weekday);
-
-                                            // Clear old habits
-                                            dataList.clear();
-
-                                            for (QueryDocumentSnapshot doc : value) {
-                                                // Get all changed habits and add to list
-                                                Habit habit = doc.toObject(Habit.class);
-                                                Log.d("CHANGE", doc.toObject(Habit.class).toString());
-                                                dataList.add(habit);
-
-                                                // Add to daily list if Habit occurs today
-                                                if (habit.isOnDay(weekday)) {
-                                                    dataList.add(habit);
-                                                }
-                                            }
-                                            habitListAdapter.notifyDataSetChanged();
+                                        // Log failure and exit
+                                        if (error != null) {
+                                            Log.w("FAILED", "Listen failed.", error);
+                                            return;
                                         }
-                                    });
-                        } else {
-                            Log.d("HABIT NOT EXISTS", "No such document");
-                        }
+
+                                        // Clear old habits
+                                        dataList.clear();
+
+                                        for (QueryDocumentSnapshot doc : value) {
+                                            // Get all changed habits and add to list
+                                            Habit habit = doc.toObject(Habit.class);
+                                            Log.d("CHANGE", doc.toObject(Habit.class).toString());
+                                            dataList.add(habit);
+                                        }
+                                        habitListAdapter.notifyDataSetChanged();
+                                    }
+                                });
                     } else {
-                        Log.d("READ FAIL", "get failed with ", task.getException());
+                        Log.d("HABIT NOT EXISTS", "No such document");
                     }
+                } else {
+                    Log.d("READ FAIL", "get failed with ", task.getException());
                 }
-            });
-        }
+            }
+        });
+
+        habitList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent1 = new Intent(FriendsHabit.this, viewOnlyHabit.class);
+                Habit habitToView = habitListAdapter.getItem(position);
+                intent1.putExtra("habit", habitToView);
+                startActivity(intent1);
+             }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/habit/viewOnlyHabit.java
@@ -0,0 +1,160 @@
+package com.example.habit;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+public class viewOnlyHabit extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    FirebaseFirestore db;
+
+    ImageButton back;
+    Button Monday;
+    Button Tuesday;
+    Button Wednesday;
+    Button Thursday;
+    Button Friday;
+    Button Saturday;
+    Button Sunday;
+    TextView title;
+    TextView reason;
+    TextView startMonth;
+    TextView startDay;
+    TextView startYear;
+    TextView endMonth;
+    TextView endDay;
+    TextView endYear;
+    ListView habitEventsListView;
+    ArrayList<HabitEvent> habitEventsDataList;
+    HabitEventList habitEventAdapter;
+
+    int Syear;
+    int Smonth;
+    int Sday;
+
+    Timestamp startTime;
+    Timestamp endTime;
+    HashMap<String, Boolean> selected_date = null;
+    ArrayList<Boolean> recurrence = new ArrayList<Boolean>(
+            Arrays.asList(false, false, false, false, false, false, false));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_habit_details);
+
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        title = findViewById(R.id.habit_title);
+        reason = findViewById(R.id.habit_reason);
+        back = findViewById(R.id.back);
+        startMonth = findViewById(R.id.start_month);
+        startDay = findViewById(R.id.start_day);
+        startYear = findViewById(R.id.start_year);
+        endMonth = findViewById(R.id.end_month);
+        endDay = findViewById(R.id.end_day);
+        endYear = findViewById(R.id.end_year);
+        Monday = findViewById(R.id.monday);
+        Tuesday = findViewById(R.id.tuesday);
+        Wednesday = findViewById(R.id.wednesday);
+        Thursday = findViewById(R.id.thursday);
+        Friday = findViewById(R.id.friday);
+        Saturday = findViewById(R.id.saturday);
+        Sunday = findViewById(R.id.sunday);
+
+        // After enter the edit interface, the existing information should replace the hints
+        User curr_user;
+
+        Habit selected_habit = getIntent().getExtras().getParcelable("habit");
+        Log.i("GOT HABIT", selected_habit.toString());
+
+        // Setup habit events list
+        habitEventsDataList = new ArrayList<>();
+        habitEventAdapter = new HabitEventList(this, habitEventsDataList, selected_habit);
+        habitEventsListView = findViewById(R.id.habit_event_list);
+        habitEventsListView.setAdapter(habitEventAdapter);
+
+        title.setText(selected_habit.getTitle());
+        reason.setText(selected_habit.getReason());
+
+        Date sDate = selected_habit.getStart();
+        Calendar cal = Calendar.getInstance(); // Add this
+        cal.setTime(sDate);
+        int sd;
+        int sm;
+        int sy;
+
+        sd = cal.get(Calendar.DAY_OF_MONTH);
+        sm = cal.get(Calendar.MONTH);
+        sy = cal.get(Calendar.YEAR);
+
+        startDay.setText(String.valueOf(sd));
+        startMonth.setText(String.valueOf(sm));
+        startYear.setText(String.valueOf(sy));
+
+        Date eDate = selected_habit.getEnd();
+        cal.setTime(eDate);
+        int ed;
+        int em;
+        int ey;
+
+        ed = cal.get(Calendar.DAY_OF_MONTH);
+        em = cal.get(Calendar.MONTH);
+        ey = cal.get(Calendar.YEAR);
+
+        endDay.setText(String.valueOf(ed));
+        endMonth.setText(String.valueOf(em));
+        endYear.setText(String.valueOf(ey));
+
+        // Get the occurrence status
+        if (selected_habit.getDaysOfWeek().get("Monday")) {
+            Monday.setBackground(getDrawable(R.drawable.round_button_clicked));
+        }
+        if (selected_habit.getDaysOfWeek().get("Tuesday")) {
+            Tuesday.setBackground(getDrawable(R.drawable.round_button_clicked));
+        }
+        if (selected_habit.getDaysOfWeek().get("Wednesday")) {
+            Wednesday.setBackground(getDrawable(R.drawable.round_button_clicked));
+        }
+        if (selected_habit.getDaysOfWeek().get("Thursday")) {
+            Thursday.setBackground(getDrawable(R.drawable.round_button_clicked));
+        }
+        if (selected_habit.getDaysOfWeek().get("Friday")) {
+            Friday.setBackground(getDrawable(R.drawable.round_button_clicked));
+        }
+        if (selected_habit.getDaysOfWeek().get("Saturday")) {
+            Saturday.setBackground(getDrawable(R.drawable.round_button_clicked));
+        }
+        if (selected_habit.getDaysOfWeek().get("Sunday")) {
+            Sunday.setBackground(getDrawable(R.drawable.round_button_clicked));
+        }
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
