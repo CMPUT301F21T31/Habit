b9a714c80cc528940e086ac33c51e656caab647d
Justin Boileau <jboileau@ualberta.ca>
2021-11-29 15:37:44 -0700

    Can view photo and location text for habit event of user you follow

--- app/src/main/java/com/example/habit/fragments/ViewOnlyHabitEventFragment.java
+++ app/src/main/java/com/example/habit/fragments/ViewOnlyHabitEventFragment.java
@@ -2,23 +2,36 @@ package com.example.habit.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.habit.entities.Habit;
 import com.example.habit.entities.HabitEvent;
 import com.example.habit.R;
+import com.example.habit.utilities.PhotoUtil;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 /**
@@ -33,10 +46,20 @@ public class ViewOnlyHabitEventFragment extends DialogFragment {
 
     private Habit habit;
     private HabitEvent habitEvent;
-    private TextView locationTextView; // TODO: @qg change this
+    private TextView locationTextView;
     private TextView commentsTextView;
 
+    ConstraintLayout noPhotoFrame;
+    ConstraintLayout photoFrame;
+    ImageView photoView;
     ImageButton backButton;
+    PhotoUtil photoUtil;
+
+    Geocoder geocoder;
+    Double latitude;
+    Double longitude;
+
+    final long ONE_MEGABYTE = 1024 * 1024;
 
     /**
      * Get a new fragment to display a HabitEvent
@@ -66,9 +89,51 @@ public class ViewOnlyHabitEventFragment extends DialogFragment {
         locationTextView = view.findViewById(R.id.location_view_text);
         commentsTextView = view.findViewById(R.id.comments_view_text);
 
-        // Set text fields
-//        locationTextView.setText(habitEvent.getLocation());
+        // Get photo and related views
+        photoFrame = view.findViewById(R.id.photoFrame);
+        noPhotoFrame = view.findViewById(R.id.noPhotoFrame);
+        photoView = view.findViewById(R.id.habitEventPhotoFrame);
+
+        // Set comment field
         commentsTextView.setText(habitEvent.getComments());
+
+        // Get location
+        latitude = habitEvent.getLatitude();
+        longitude = habitEvent.getLongitude();
+        geocoder = new Geocoder(getActivity(), Locale.getDefault());
+
+        // Get and display more info about the location
+        if (latitude != null && longitude != null) {
+            try {
+                List<Address> addresses = null;
+                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                Address address = addresses.get(0);
+                locationTextView.setText(address.getAddressLine(0));
+                System.out.println("Got address");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Set photo if there is one
+        PhotoUtil photoUtil = new PhotoUtil();
+        if (habitEvent.getPhotoPath() != null) {
+            StorageReference imageRef = photoUtil.storageRef.child(habitEvent.getPhotoPath());
+            imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    // Data for "images/island.jpg" is returns, use this as needed
+                    Bitmap photo = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    noPhotoFrame.setVisibility(View.INVISIBLE);
+                    photoView.setImageBitmap(photo);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+                }
+            });
+        }
 
         // Set back button
         backButton = view.findViewById(R.id.backHabitEventButton);
